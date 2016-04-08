95068ac2e1757117f8c6e34b216078933a21a903
wenjiangtao <wenjiangtao@outlook.com>
2016-04-08 10:13:10 +0800

    finish basic

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour 
+{
+    public delegate void GetScoreHandler(int score);
+    public delegate void WinHandler();
+    public static event GetScoreHandler OnGetScore;
+    public static event WinHandler OnWin;
+    
+    public float speed = 300f;
+    public int winScore = 12;
+    
+    private int _score;
+
+    Rigidbody playerRigidbody;
+    Vector3 movement;
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        _score = 0;
+    }
+
+	void Update()
+    {
+        movement = new Vector3(
+            Input.GetAxis("Horizontal"),
+            0.0f,
+            Input.GetAxis("Vertical")
+        ) * speed;
+    }
+    
+    void FixedUpdate()
+    {
+        playerRigidbody.AddForce(movement * Time.fixedDeltaTime);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PickUp")
+        {
+            Destroy(other.gameObject);
+            
+            _score++;
+            DispatchGetScoreEvent();
+            if (_score == winScore){
+                DispatchWinEvent();
+            }
+        }
+    }
+
+    private void DispatchWinEvent()
+    {
+        if (OnWin != null)
+        {
+            OnWin();
+        }
+    }
+
+    private void DispatchGetScoreEvent()
+    {
+        if (OnGetScore != null)
+        {
+            OnGetScore(_score);
+        }
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    public Text winText;
+    public Text scoreText;
+    
+    void Start()
+    {
+        PlayerController.OnGetScore += OnGetScore;
+        PlayerController.OnWin += OnWin;
+    }
+    
+    void OnGetScore(int score)
+    {
+        Debug.Log(score);
+        scoreText.text = "Score: " + score;
+    }
+    
+    void OnWin()
+    {
+        winText.gameObject.SetActive(true);
+    }
+}
